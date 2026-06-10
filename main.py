038ab26a6cc5c6cda2170b7b978bca8f6aa79a74
lisarchick <kochanov.ps@gmail.com>
2026-06-10 20:55:31 +0300

    my second project

---
+++ main.py
@@ -0,0 +1,12 @@
+
+class Students:
+    # def __init__(self, names):
+    #     self.names = names
+
+    def fnames(self, name):
+        for i in name:
+            print(i)
+
+
+g = Students()
+g.fnames("i")
